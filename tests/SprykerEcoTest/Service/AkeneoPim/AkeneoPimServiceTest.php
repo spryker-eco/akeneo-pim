96baf587bd8942e5484dbd80ed4d0c2ba70380bb
vhrychenko <aptah199494@gmail.com>
2018-06-20 19:45:50 +0300

    ECO-784 Update tests

--- tests/SprykerEcoTest/Service/AkeneoPim/AkeneoPimServiceTest.php
+++ tests/SprykerEcoTest/Service/AkeneoPim/AkeneoPimServiceTest.php
@@ -16,8 +16,15 @@ use SprykerEco\Service\AkeneoPim\AkeneoPimServiceInterface;
 use SprykerEco\Service\AkeneoPim\Api\Adapter\AdapterFactory;
 use SprykerEco\Service\AkeneoPim\Api\Adapter\AdapterFactoryInterface;
 use SprykerEco\Service\AkeneoPim\Api\Adapter\ApiAdapterInterface;
+use SprykerEco\Service\AkeneoPim\Api\Adapter\Attributes\AttributeApiAdapter;
+use SprykerEco\Service\AkeneoPim\Api\Adapter\Attributes\AttributeOptionApiAdapter;
+use SprykerEco\Service\AkeneoPim\Api\Adapter\Attributes\AttributeOptionApiAdapterInterface;
 use SprykerEco\Service\AkeneoPim\Api\Adapter\Category\CategoryApiAdapter;
+use SprykerEco\Service\AkeneoPim\Api\Adapter\Family\FamilyApiAdapter;
+use SprykerEco\Service\AkeneoPim\Api\Adapter\Family\FamilyVariantApiAdapter;
+use SprykerEco\Service\AkeneoPim\Api\Adapter\Family\FamilyVariantApiAdapterInterface;
 use SprykerEco\Service\AkeneoPim\Api\Adapter\Product\ProductApiAdapter;
+use SprykerEco\Service\AkeneoPim\Api\Adapter\Product\ProductModelApiAdapter;
 
 /**
  * @group SprykerEcoTest
@@ -39,6 +46,36 @@ class AkeneoPimServiceTest extends Unit
         'product3',
     ];
 
+    protected const ATTRIBUTE_DATA = [
+        'attribute1',
+        'attribute2',
+        'attribute3',
+    ];
+
+    protected const FAMILY_DATA = [
+        'family1',
+        'family2',
+        'family3',
+    ];
+
+    protected const FAMILY_VARIANTS_DATA = [
+        'familyVariants1',
+        'familyVariants2',
+        'familyVariants3',
+    ];
+
+    protected const ATTRIBUTE_OPTIONS_DATA = [
+        'option1',
+        'option2',
+        'option3',
+    ];
+
+    protected const PRODUCT_MODELS_DATA = [
+        'model1',
+        'model2',
+        'model3',
+    ];
+
     /**
      * @return void
      */
@@ -57,6 +94,41 @@ class AkeneoPimServiceTest extends Unit
         $service = $this->prepareService();
 
         $this->assertEquals(static::CATEGORY_DATA, $service->getAllCategories());
+    }
+
+    public function testGetAllAttributes(): void
+    {
+        $service = $this->prepareService();
+
+        $this->assertEquals(static::ATTRIBUTE_DATA, $service->getAllAttributes());
+    }
+
+    public function testGetAllFamilies(): void
+    {
+        $service = $this->prepareService();
+
+        $this->assertEquals(static::FAMILY_DATA, $service->getAllFamilies());
+    }
+
+    public function testGetFamilyVariants(): void
+    {
+        $service = $this->prepareService();
+
+        $this->assertEquals(static::FAMILY_VARIANTS_DATA, $service->getAllFamilyVariants('code'));
+    }
+
+    public function testGetAllAttributeOptions(): void
+    {
+        $service = $this->prepareService();
+
+        $this->assertEquals(static::ATTRIBUTE_OPTIONS_DATA, $service->getAllAttributeOptions('code'));
+    }
+
+    public function testGetAllProductModels(): void
+    {
+        $service = $this->prepareService();
+
+        $this->assertEquals(static::PRODUCT_MODELS_DATA, $service->getAllProductModels());
     }
 
     /**
@@ -110,12 +182,25 @@ class AkeneoPimServiceTest extends Unit
     protected function createAdapterFactoryMock(): AdapterFactoryInterface
     {
         $factory = $this->getMockBuilder(AdapterFactory::class)
-            ->setMethods(['createProductApiAdapter', 'createCategoryApiAdapter'])
+            ->setMethods([
+                'createProductApiAdapter',
+                'createCategoryApiAdapter',
+                'createAttributeApiAdapter',
+                'createFamilyApiAdapter',
+                'createFamilyVariantApiAdapter',
+                'createAttributeOptionApiAdapter',
+                'createProductModelApiAdapter',
+            ])
             ->disableOriginalConstructor()
             ->getMock();
 
         $factory->method('createCategoryApiAdapter')->willReturn($this->createCategoryApiAdapterMock());
         $factory->method('createProductApiAdapter')->willReturn($this->createProductApiAdapterMock());
+        $factory->method('createAttributeApiAdapter')->willReturn($this->createAttributeApiAdapterMock());
+        $factory->method('createFamilyApiAdapter')->willReturn($this->createFamilyApiAdapterMock());
+        $factory->method('createFamilyVariantApiAdapter')->willReturn($this->createFamilyVariantApiAdapterMock());
+        $factory->method('createAttributeOptionApiAdapter')->willReturn($this->createAttributeOptionApiAdapterMock());
+        $factory->method('createProductModelApiAdapter')->willReturn($this->createProductModelApiAdapterMock());
 
         return $factory;
     }
@@ -146,6 +231,81 @@ class AkeneoPimServiceTest extends Unit
             ->getMock();
 
         $adapter->method('all')->willReturn(static::PRODUCT_DATA);
+
+        return $adapter;
+    }
+
+    /**
+     * @return \SprykerEco\Service\AkeneoPim\Api\Adapter\ApiAdapterInterface
+     */
+    protected function createAttributeApiAdapterMock(): ApiAdapterInterface
+    {
+        $adapter = $this->getMockBuilder(AttributeApiAdapter::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['all'])
+            ->getMock();
+
+        $adapter->method('all')->willReturn(static::ATTRIBUTE_DATA);
+
+        return $adapter;
+    }
+
+    /**
+     * @return \SprykerEco\Service\AkeneoPim\Api\Adapter\ApiAdapterInterface
+     */
+    protected function createFamilyApiAdapterMock(): ApiAdapterInterface
+    {
+        $adapter = $this->getMockBuilder(FamilyApiAdapter::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['all'])
+            ->getMock();
+
+        $adapter->method('all')->willReturn(static::FAMILY_DATA);
+
+        return $adapter;
+    }
+
+    /**
+     * @return \SprykerEco\Service\AkeneoPim\Api\Adapter\Family\FamilyVariantApiAdapterInterface
+     */
+    protected function createFamilyVariantApiAdapterMock(): FamilyVariantApiAdapterInterface
+    {
+        $adapter = $this->getMockBuilder(FamilyVariantApiAdapter::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['all'])
+            ->getMock();
+
+        $adapter->method('all')->willReturn(static::FAMILY_VARIANTS_DATA);
+
+        return $adapter;
+    }
+
+    /**
+     * @return \SprykerEco\Service\AkeneoPim\Api\Adapter\Attributes\AttributeOptionApiAdapterInterface
+     */
+    protected function createAttributeOptionApiAdapterMock(): AttributeOptionApiAdapterInterface
+    {
+        $adapter = $this->getMockBuilder(AttributeOptionApiAdapter::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['all'])
+            ->getMock();
+
+        $adapter->method('all')->willReturn(static::ATTRIBUTE_OPTIONS_DATA);
+
+        return $adapter;
+    }
+
+    /**
+     * @return \SprykerEco\Service\AkeneoPim\Api\Adapter\ApiAdapterInterface
+     */
+    protected function createProductModelApiAdapterMock(): ApiAdapterInterface
+    {
+        $adapter = $this->getMockBuilder(ProductModelApiAdapter::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['all'])
+            ->getMock();
+
+        $adapter->method('all')->willReturn(static::PRODUCT_MODELS_DATA);
 
         return $adapter;
     }
