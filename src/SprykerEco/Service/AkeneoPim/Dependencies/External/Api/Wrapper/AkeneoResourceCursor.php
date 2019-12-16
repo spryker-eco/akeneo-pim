f9a8c6141200af2cb3fb3f94d86e30a5af7b8e0f
Dmitri Kadykov <dmitri.kadykov@spryker.com>
2019-12-16 12:03:47 +0000

    code sniffer fix

--- src/SprykerEco/Service/AkeneoPim/Dependencies/External/Api/Wrapper/AkeneoResourceCursor.php
+++ src/SprykerEco/Service/AkeneoPim/Dependencies/External/Api/Wrapper/AkeneoResourceCursor.php
@@ -25,7 +25,7 @@ class AkeneoResourceCursor implements AkeneoResourceCursorInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      *
      * @return int
      */
@@ -36,7 +36,7 @@ class AkeneoResourceCursor implements AkeneoResourceCursorInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      *
      * @return mixed
      */
@@ -47,7 +47,7 @@ class AkeneoResourceCursor implements AkeneoResourceCursorInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      *
      * @return void
      */
@@ -58,7 +58,7 @@ class AkeneoResourceCursor implements AkeneoResourceCursorInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      *
      * @return mixed
      */
@@ -69,7 +69,7 @@ class AkeneoResourceCursor implements AkeneoResourceCursorInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      *
      * @return bool
      */
@@ -80,7 +80,7 @@ class AkeneoResourceCursor implements AkeneoResourceCursorInterface
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      *
      * @return void
      */
